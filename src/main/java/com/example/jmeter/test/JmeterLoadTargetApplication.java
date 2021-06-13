42b70d435d0433cbd6c1c4ba0087ceaf3db714cd
Chen Riang Lam <chenriang.lam@gbgplc.com>
2021-06-13 16:57:30 +0800

    add simple test endpoint

---
+++ src/main/java/com/example/jmeter/test/JmeterLoadTargetApplication.java
@@ -0,0 +1,12 @@
+package com.example.jmeter.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterLoadTargetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmeterLoadTargetApplication.class, args);
+    }
+}
